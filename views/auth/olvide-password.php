eeb162025e140912d258a09d3515f36bfd1696b9
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-01-07 12:24:56 -0400

    Vistas crear-cuenta  y olvide-password  creadas
    Vistas creadas y agregadas al LoginController

---
+++ views/auth/olvide-password.php
@@ -0,0 +1,24 @@
+<h1 class="nombre-pagina">Olvide Password</h1>
+
+<p class="descripcion-pagina">Reestablece tu password escribiendo tu email a continuación</p>
+
+<form class="formulario" action="/olvide" method="POST">
+
+    <div class="campo">
+        <label for="email">E-mail</label>
+        <input 
+        type="email" 
+        id="email" 
+        name="email" 
+        placeholder="Tu E-mail"
+        
+        />
+    </div>
+
+    <input type="submit" class="boton" value="Enviar">
+</form>
+
+<div class="acciones">
+    <a href="/">¿Ya tienes cuenta? Iniciar Sesión</a>
+    <a href="/crear-cuenta">¿Aún no tienes cuenta? Crear una</a>
+</div>
